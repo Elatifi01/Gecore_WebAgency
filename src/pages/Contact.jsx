c114da3e20ce68f97ea618a088db03ed4cf4ff1a
Elatifi01 <elatifi.haitam01@gmail.com>
2026-03-18 15:59:46 -0700

    fix contact infos

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -118,9 +118,9 @@ const Contact = () => {
           />
         </svg>
       ),
-      title: "Business Hours",
-      details: "Monday - Friday: 9am - 6pm",
-      subdetails: "Saturday - Sunday: Closed",
+      title: "Heures d’Ouverture",
+      details: "Lundi - Vendredi : 9h - 18h",
+      subdetails: "Samedi - Dimanche : Fermé",
     },
   ];
 
@@ -359,7 +359,9 @@ const Contact = () => {
                       d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7"
                     />
                   </svg>
-                  <p className="text-gray-500 font-roboto">Map Location</p>
+                  <p className="text-gray-500 font-roboto">
+                    Localisation sur la Carte
+                  </p>
                 </div>
               </motion.div>
             </motion.div>
